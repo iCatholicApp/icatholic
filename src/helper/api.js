ac65f05fc7938f45a3fc49f71926a4fc84d54fa1
ozdv <cam@ozdv.me>
2023-01-26 08:38:58 -0700

    better api error handling and comments for future development

--- src/helper/api.js
+++ src/helper/api.js
@@ -8,12 +8,30 @@ const axiosClient = axios.create({
     headers: { "api-key": API_KEY },
 });
 
+// response interceptor
 axiosClient.interceptors.response.use(
     function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
         return response;
     },
     function (error) {
-        console.log("ERROR", error.response.data);
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+        }
+        console.log(error.config);
         return Promise.reject(error);
     }
 );
