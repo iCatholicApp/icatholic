8fff3e3202080d754c6b6d911be4eb6352ad992a
ozdv <cam@ozdv.me>
2023-02-23 19:49:28 -0700

    style fixes

--- src/screens/PrayerListScreen.js
+++ src/screens/PrayerListScreen.js
@@ -1,65 +1,60 @@
 import React, { useState } from "react";
-
 import { StyleSheet, ScrollView, TextInput } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { WideButton } from "../components";
 import colors from "../theme/colors";
 import prayers from "../constants/prayers";
-import { useNavigation } from "@react-navigation/native";
 
 export default function PrayerListScreen() {
-    const navigation = useNavigation();
-    const [searchString, setSearchString] = useState("");
+  const navigation = useNavigation();
+  const [searchString, setSearchString] = useState("");
 
-    return (
-        <ScrollView style={styles.container}>
-            <TextInput
-                placeholder="Search"
-                value={searchString}
-                onChangeText={(text) => setSearchString(text)}
-                style={styles.searchInput}
-            />
+  return (
+    <ScrollView style={styles.container}>
+      <TextInput
+        placeholder="Search"
+        value={searchString}
+        onChangeText={(text) => setSearchString(text)}
+        style={styles.searchInput}
+      />
 
-            {prayers
-                .filter((prayer) => {
-                    if (searchString === "") {
-                        return prayers;
-                    } else if (
-                        prayer.label
-                            .toLowerCase()
-                            .includes(searchString.toLowerCase())
-                    ) {
-                        return prayers;
-                    }
-                })
-                .map((prayer, i) => (
-                    <WideButton
-                        key={i}
-                        text={prayer.label}
-                        buttonStyle={styles.buttonStyle}
-                        onPress={() =>
-                            navigation.navigate("Prayer", { prayer })
-                        }
-                    />
-                ))}
-        </ScrollView>
-    );
+      {prayers
+        .filter((prayer) => {
+          if (searchString === "") {
+            return prayers;
+          } else if (
+            prayer.label.toLowerCase().includes(searchString.toLowerCase())
+          ) {
+            return prayers;
+          }
+        })
+        .map((prayer, i) => (
+          <WideButton
+            key={i}
+            text={prayer.label}
+            buttonStyle={styles.buttonStyle}
+            onPress={() => navigation.navigate("Prayer", { prayer })}
+          />
+        ))}
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colors.neutral95,
-        padding: 15,
-    },
-    buttonStyle: {
-        marginBottom: 15,
-    },
-    searchInput: {
-        width: "100%",
-        backgroundColor: colors.white,
-        marginBottom: 15,
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: colors.neutral95,
+    padding: 15,
+  },
+  buttonStyle: {
+    marginBottom: 15,
+  },
+  searchInput: {
+    width: "100%",
+    backgroundColor: colors.white,
+    marginBottom: 15,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
 });
